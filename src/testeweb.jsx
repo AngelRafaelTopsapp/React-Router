7403b73b7f7e4b5203b552e0d582c53fd5cfd8b2
AngelRafaelTopsapp <rafael.souza@topsapp.com.br>
2024-08-13 11:13:17 -0400

    Update testeweb.jsx

--- src/testeweb.jsx
+++ src/testeweb.jsx
@@ -5,6 +5,7 @@ createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <div>
      <button class="bg-emerald-400"> eu sou um botao idiota 1</button>
+     <video> era pra ter um video</video>
      <button class="bg-yellow-400"> eu sou um botao idiota 2</button>
     </div>
   </React.StrictMode>
